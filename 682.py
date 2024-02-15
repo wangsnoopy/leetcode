4f9e24661a8b99e47543ec3c1c2e10fa40c9f30f
Austin Wang <wangaus1997@gmail.com>
2024-02-14 23:54:23 -0800

    682. Baseball Game

---
+++ 682.py
@@ -0,0 +1,19 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        # a record stack to record the score
+        # interate all the ele in ops
+        record = []
+        for i in operations:
+            if i == '+':
+                score = record[-1] + record[-2]
+                record.append(score)
+            elif i == 'C':
+                record.pop()
+            elif i == 'D':
+                score = record[-1] * 2
+                record.append(score)
+            # if i is num, record i
+            else:
+                record.append(int(i))
+
+        return sum(record)
