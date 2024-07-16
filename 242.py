e88e0f84bbbe718bd3ed4dfadcb82bd0fb203d13
wangsnoopy <wangaus1997@gmail.com>
2024-07-15 23:15:05 -0700

    242

--- 242.py
+++ 242.py
@@ -1,23 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        # dic to record ele show in s
-        show = {}
-        for ele in s:
-            if ele not in show:
-                show[ele] = 1
-            # if show, + 1
-            else:
-                show[ele] += 1
-        # count the ele show time, if show in t, - 1
-        # Iterate t
-        for i in t:
-            if i in show:
-                if show[i] > 0:
-                    show[i] -= 1
-                else:
-                    return False
-            else:
+        seen = defaultdict(int)
+
+        for cr in s:
+            seen[cr] += 1
+        
+        for cr in t:
+            seen[cr] -= 1
+        
+        for val in seen.values():
+            if val != 0:
                 return False
+        
         return True
